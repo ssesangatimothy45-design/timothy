6fc0b73872d4b4ecf62a9f0a779d352ee03597a2
ssesangatimothy45@gmail.com <2400720279@mubs.ac.ug>
2026-09-22 16:55:35 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\HomeController;
-use App\Http\Controller\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
 
 Route::get('/',[HomeController::class,'index'])->name('index');
 Route::get('/contact',[HomeController::class,'contact'])->name('contact');
